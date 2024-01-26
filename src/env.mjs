0659ff3f3301655702cff6b4f12d20b3cc3322e4
ramunecroft <ramunecroft@gmail.com>
2024-01-26 22:55:40 +0900

    direct url

--- src/env.mjs
+++ src/env.mjs
@@ -9,6 +9,7 @@ export const env = createEnv({
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]).default("development"),
     DATABASE_URL: z.string().url().optional(),
+    DIRECT_URL: z.string().url().optional()
   },
 
   /**
@@ -27,6 +28,7 @@ export const env = createEnv({
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     NODE_ENV: process.env.NODE_ENV,
     DATABASE_URL: process.env.DATABASE_URL,
+    DIRECT_URL: process.env.DIRECT_URL,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
