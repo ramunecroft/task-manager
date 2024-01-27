33f143e8c588b0f416854aefae57def1bad69b55
ramunecroft <ramunecroft@gmail.com>
2024-01-27 23:51:22 +0900

    add return type

--- src/components/search-button.tsx
+++ src/components/search-button.tsx
@@ -7,7 +7,7 @@ type SearchButtonProps = {
 export const SearchButton: React.FC<SearchButtonProps> = ({onClick}) => {
   return (
     <Button
-      onClick={onClick()}
+      onClick={() => onClick()}
       className="relative inline-flex h-8 w-full items-center justify-start whitespace-nowrap rounded-[0.5rem] border border-input 
     bg-background px-4 py-2 text-sm font-normal text-muted-foreground shadow-none transition-colors 
     hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-1 
