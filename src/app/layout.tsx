d6a2b5e04ecc6a51e76322a93db364b3dc1c12a9
ramunecroft <ramunecroft@gmail.com>
2024-01-31 23:05:25 +0900

    footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type {Metadata, Viewport} from "next";
 import "@/styles/globals.css";
 import {cn} from "@/lib/utils";
 import {fontSans} from "@/lib/fonts";
+import {SiteFooter} from "@/components/site-footer";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -39,7 +40,7 @@ export default function RootLayout({children}: Readonly<RootLayoutProps>) {
             <div className="relative flex min-h-screen flex-col bg-background">
               <SiteHeader />
               <main className="flex-1">{children}</main>
-              {/* <SiteFooter /> */}
+              <SiteFooter />
             </div>
           </div>
         </Provider>
