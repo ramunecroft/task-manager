97a4c36ab1a9ea9231e7d75eaa1279351bdc9682
ramunecroft <ramunecroft@gmail.com>
2024-02-11 19:02:47 +0900

    test

--- src/client/api/task.ts
+++ src/client/api/task.ts
@@ -1,7 +1,7 @@
 import {apiFetch} from "@/lib/utils";
 
 export const getTasks = async () => {
-  const url = `${process.env.NEXT_PUBLIC_API_URL}/api/task`;
+  const url = `/api/task`;
 
   const res = await apiFetch<Response, undefined>(url, {
     headers: {
