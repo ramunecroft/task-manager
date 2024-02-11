3a1ada44772136ee694c247fc7806b11f2b0099c
ramunecroft <ramunecroft@gmail.com>
2024-02-11 22:31:57 +0900

    test

--- src/components/task-section.tsx
+++ src/components/task-section.tsx
@@ -1,20 +1,25 @@
 "use client";
 
-import {TaskCard} from "@/components/task-card";
-import {type Task} from "@/server/schema";
-import {mutateAtom, postAtom, taskStore} from "@/store/task";
+// import {mutateAtom, postAtom, taskStore} from "@/store/task";
 import {Provider, useAtomValue, useSetAtom} from "jotai";
 import React, {type DragEvent} from "react";
+import {getTasks} from "../client/api/task";
+import {type Task} from "@/server/schema";
+import {TaskCard} from "@/components/task-card";
 
 type TaskSectionType = {
   status: Task["status"];
 };
 
-export const TaskSection = ({status}: TaskSectionType) => {
-  const taskList = useAtomValue(postAtom);
-  const mutate = useSetAtom(mutateAtom);
+export const TaskSection = async ({status}: TaskSectionType) => {
+  // const taskList = useAtomValue(postAtom);
+  // const mutate = useSetAtom(mutateAtom);
 
-  if (!taskList) return;
+  const response = await getTasks();
+
+  const taskList = (await response.json()) as Task[];
+
+  if (!response) return;
   const handleDrop = async (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
     const draggingTask = taskList?.find(
@@ -32,7 +37,6 @@ export const TaskSection = ({status}: TaskSectionType) => {
         status,
       }),
     });
-    await mutate(taskList);
   };
 
   const handleDragOver = (e: DragEvent<HTMLDivElement>) => {
@@ -40,29 +44,27 @@ export const TaskSection = ({status}: TaskSectionType) => {
   };
 
   return (
-    <Provider store={taskStore}>
-      <div
-        className="bg-gray-100 dark:bg-gray-800/40"
-        onDrop={e => handleDrop(e)}
-        onDragOver={e => handleDragOver(e)}>
-        <div className="flex flex-col items-start justify-center space-y-2">
-          <h3 className="text-md mx-4 my-2 font-semibold text-gray-500 dark:text-white">
-            {status.split("_").join(" ")}
-          </h3>
-          {Array.isArray(taskList) &&
-            taskList
-              .filter(task => task.status === status)
-              .map((task, index) => (
-                <TaskCard
-                  key={task.ticketCode}
-                  description={task.description}
-                  ticketCode={task.ticketCode}
-                  priority={task.priority}
-                  voteCount={task.voteCount}
-                />
-              ))}
-        </div>
+    <div
+      className="bg-gray-100 dark:bg-gray-800/40"
+      onDrop={e => handleDrop(e)}
+      onDragOver={e => handleDragOver(e)}>
+      <div className="flex flex-col items-start justify-center space-y-2">
+        <h3 className="text-md mx-4 my-2 font-semibold text-gray-500 dark:text-white">
+          {status.split("_").join(" ")}
+        </h3>
+        {Array.isArray(taskList) &&
+          taskList
+            .filter(task => task.status === status)
+            .map((task, index) => (
+              <TaskCard
+                key={task.ticketCode}
+                description={task.description}
+                ticketCode={task.ticketCode}
+                priority={task.priority}
+                voteCount={task.voteCount}
+              />
+            ))}
       </div>
-    </Provider>
+    </div>
   );
 };
--- src/store/task.ts
+++ src/store/task.ts
@@ -1,28 +1,28 @@
-import {getTasks} from "@/client/api/task";
-import {type Task} from "@/server/schema";
-import {atom, createStore} from "jotai";
+// import {getTasks} from "@/client/api/task";
+// import {type Task} from "@/server/schema";
+// import {atom, createStore} from "jotai";
 
-export const messageAtom = atom("");
+// export const messageAtom = atom("");
 
-const mutationResultAtom = atom<Task[] | null>(null);
-mutationResultAtom.debugPrivate = true;
+// const mutationResultAtom = atom<Task[] | null>(null);
+// mutationResultAtom.debugPrivate = true;
 
-export const postAtom = atom<Promise<Task[]>>(async get => {
-  const mutationResult = get(mutationResultAtom);
-  if (mutationResult) {
-    return mutationResult;
-  }
-  const res = await getTasks();
+// export const postAtom = atom<Promise<Task[]>>(async get => {
+//   const mutationResult = get(mutationResultAtom);
+//   if (mutationResult) {
+//     return mutationResult;
+//   }
+//   const res = await getTasks();
 
-  const data = (await res.json()) as Task[];
-  return data;
-});
-postAtom.debugLabel = "taskList";
+//   const data = (await res.json()) as Task[];
+//   return data;
+// });
+// postAtom.debugLabel = "taskList";
 
-export const mutateAtom = atom(null, async (get, set, taskList) => {
-  const res = await getTasks();
-  const data = (await res.json()) as Task[];
-  set(mutationResultAtom, data);
-});
+// export const mutateAtom = atom(null, async (get, set, taskList) => {
+//   const res = await getTasks();
+//   const data = (await res.json()) as Task[];
+//   set(mutationResultAtom, data);
+// });
 
-export const taskStore = createStore();
+// export const taskStore = createStore();
