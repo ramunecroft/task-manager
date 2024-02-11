f0e979ceaeea8712f5f778803511dbaf532bbf9d
ramunecroft <ramunecroft@gmail.com>
2024-02-12 00:00:05 +0900

    remove

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,7 +4,7 @@ import {migrate} from "drizzle-orm/postgres-js/migrator";
 import * as schema from "@/server/schema";
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
-export const client = postgres(process.env.DATABASE_URL);
+export const client = postgres(process.env.DATABASE_URL ?? "");
 
 export const db = drizzle(client, {logger: true, schema});
 
