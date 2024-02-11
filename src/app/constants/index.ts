0ddf439c61f71880f7b5c31e344f8b98cb0bac0a
ramunecroft <ramunecroft@gmail.com>
2024-02-11 20:55:20 +0900

    test

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -6,5 +6,4 @@ export const Priority = {
   Highest: "highest",
 } as const;
 
-export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL;
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
