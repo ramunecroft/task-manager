b2bf7d78a3a6a12bc35c2245544edaffe96ae1eb
ramunecroft <ramunecroft@gmail.com>
2024-03-05 20:40:59 +0900

    add: auth schema

--- src/server/schema.ts
+++ src/server/schema.ts
@@ -1,5 +1,15 @@
+import {type AdapterAccount} from "@auth/core/adapters";
 import {type InferInsertModel, type InferSelectModel, relations} from "drizzle-orm";
-import {index, integer, pgEnum, pgTable, serial, text} from "drizzle-orm/pg-core";
+import {
+  index,
+  integer,
+  pgEnum,
+  pgTable,
+  primaryKey,
+  serial,
+  text,
+  timestamp,
+} from "drizzle-orm/pg-core";
 import {createInsertSchema, createSelectSchema} from "drizzle-zod";
 import {type z} from "zod";
 
@@ -19,9 +29,52 @@ export const statusEnum = pgEnum("status", [
   "DONE",
 ]);
 
+export const accounts = pgTable(
+  "account",
+  {
+    userId: text("userId")
+      .notNull()
+      .references(() => users.id, {onDelete: "cascade"}),
+    type: text("type").$type<AdapterAccount["type"]>().notNull(),
+    provider: text("provider").notNull(),
+    providerAccountId: text("providerAccountId").notNull(),
+    refresh_token: text("refresh_token"),
+    access_token: text("access_token"),
+    expires_at: integer("expires_at"),
+    token_type: text("token_type"),
+    scope: text("scope"),
+    id_token: text("id_token"),
+    session_state: text("session_state"),
+  },
+  account => ({
+    compoundKey: primaryKey({columns: [account.provider, account.providerAccountId]}),
+  })
+);
+
+export const sessions = pgTable("session", {
+  sessionToken: text("sessionToken").notNull().primaryKey(),
+  userId: text("userId")
+    .notNull()
+    .references(() => users.id, {onDelete: "cascade"}),
+  expires: timestamp("expires", {mode: "date"}).notNull(),
+});
+
+export const verificationTokens = pgTable(
+  "verificationToken",
+  {
+    identifier: text("identifier").notNull(),
+    token: text("token").notNull(),
+    expires: timestamp("expires", {mode: "date"}).notNull(),
+  },
+  vt => ({
+    compoundKey: primaryKey({columns: [vt.identifier, vt.token]}),
+  })
+);
+
 export const users = pgTable("users", {
   id: serial("id").primaryKey(),
   email: text("email").notNull(),
+  emailVerified: timestamp("emailVerified", {mode: "date"}),
   role: roleEnum("user").notNull(),
 });
 
