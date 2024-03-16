d67f2f3e7dba226c3c6d804b00de2237ac566b86
ramunecroft <ramunecroft@gmail.com>
2024-03-16 13:14:30 +0900

    fix: max connection issue

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,11 +1,26 @@
 import * as schema from "@/server/db/schema";
 import {drizzle} from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-import {env} from "process";
+import {env} from "@/env.mjs";
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
-export const client = postgres(env.DATABASE_URL || "", {
-  connect_timeout: 300,
-});
+// @see {https://github.com/orgs/supabase/discussions/18986}
 
-export const db = drizzle(client, {logger: true, schema});
+let client: postgres.Sql;
+
+if (process.env.NODE_ENV === "production") {
+  client = postgres(env.DATABASE_URL, {prepare: false});
+} else {
+  const globalConnection = global as typeof globalThis & {
+    connection: postgres.Sql;
+  };
+
+  if (!globalConnection.connection) {
+    globalConnection.connection = postgres(env.DATABASE_URL, {
+      prepare: false,
+    });
+  }
+
+  client = globalConnection.connection;
+}
+
+export const db = drizzle(client, {schema, logger: env.NODE_ENV === "development"});
